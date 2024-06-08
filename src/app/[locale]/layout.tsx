dbca53c12caa51f0715e371f219c35684b6ccdd7
Tancredi Trugenberger <tancredi.trugenberger@gmail.com>
2024-06-08 19:28:09 +0200

    Change layout meta

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -29,7 +29,7 @@ const Layout = async ({
 
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, maximum-scale=1"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
         />
       </head>
 
