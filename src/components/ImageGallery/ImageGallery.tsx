cd4ea919e036feb1a44b6505177a7f38c7737faf
Tancredi Trugenberger <tancredi.trugenberger@gmail.com>
2024-06-08 17:30:17 +0200

    Add breakpoints

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -4,13 +4,13 @@ import { FunctionComponent } from "react";
 export const ImageGallery: FunctionComponent<{ images: StaticImageData[] }> = ({
   images,
 }) => (
-  <div className="-mx-10 max-sm:!mx-0 hidden">
+  <div className="-mx-10 max-md:!-mx-6 max-lg:mx-3">
     {images.map((image, index) => (
       <Image
         key={index}
         src={image}
         alt={`Photo ${index}`}
-        className="w-full rounded-xl"
+        className="w-full md:rounded-xl"
       />
     ))}
   </div>
