35bd46dbca1268eb647ad375dea36a451d188211
Tancredi Trugenberger <tancredi.trugenberger@gmail.com>
2024-06-08 18:35:45 +0200

    More whitespace

--- src/components/ContentSection/ContentSection.tsx
+++ src/components/ContentSection/ContentSection.tsx
@@ -4,7 +4,7 @@ import { FunctionComponent, PropsWithChildren } from "react";
 export const ContentSection: FunctionComponent<
   PropsWithChildren<{ icon?: Icon; title?: string }>
 > = ({ icon: Icon, title, children }) => (
-  <div className="my-6">
+  <div className="my-12">
     {!!title && (
       <h1 className="flex flex-col gap-2 items-center text-secondary text-2xl mb-3">
         {!!Icon && <Icon size={40} stroke={0.7} />}
