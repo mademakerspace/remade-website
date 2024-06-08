a23d94b84ee0c5000fb3dc14226b56b724602788
Tancredi Trugenberger <tancredi.trugenberger@gmail.com>
2024-06-08 19:21:50 +0200

    Fix user scalable meta

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -29,7 +29,7 @@ const Layout = async ({
 
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no user-scalable=no"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no"
         />
       </head>
 
