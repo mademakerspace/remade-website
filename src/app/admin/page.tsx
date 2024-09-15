0f0d82e6fa52bbfae76177d2d20bc9debae2b863
Tancredi Trugenberger <tancredi.trugenberger@gmail.com>
2024-09-15 15:05:31 +0200

    Add basic Decap CMS admin

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,17 @@
+export default function AdminPage() {
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="noindex" />
+        <title>Admin | ReMade - BCN Maker Space</title>
+        <link href="/decap-config.yml" type="text/yaml" rel="cms-config-url" />
+      </head>
+
+      <body>
+        <script src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js" />
+      </body>
+    </html>
+  );
+}
