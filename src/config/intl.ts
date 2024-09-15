98c46ae089d837ae3ac9352250e30891c1b8e251
Tancredi Trugenberger <tancredi.trugenberger@gmail.com>
2024-09-15 14:45:32 +0200

    Move translation files

--- src/config/intl.ts
+++ src/config/intl.ts
@@ -5,9 +5,12 @@ import { redirect } from "next/navigation";
 import { DEFAULT_LOCALE, Locale } from "@/types/intl";
 
 export const dictionaries = {
-  en: () => import("@/dictionaries/en.json").then((module) => module.default),
-  es: () => import("@/dictionaries/es.json").then((module) => module.default),
-  ca: () => import("@/dictionaries/ca.json").then((module) => module.default),
+  en: () =>
+    import("@/dictionaries/index.en.json").then((module) => module.default),
+  es: () =>
+    import("@/dictionaries/index.es.json").then((module) => module.default),
+  ca: () =>
+    import("@/dictionaries/index.ca.json").then((module) => module.default),
 } as const;
 
 const getDictionary = async (locale: Locale) => dictionaries[locale]();
