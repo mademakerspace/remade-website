6ea3cdd1d2a31f9a5fd97e568ae2b6c1955dee62
Monkey Do <MonkeyDo@users.noreply.github.com>
2025-02-05 22:40:41 +0100

    Fix wiki link text
    That'll teach me to push to master without testing (surely)

--- src/layout/MainNav/MainNav.tsx
+++ src/layout/MainNav/MainNav.tsx
@@ -64,7 +64,7 @@ export const MainNav: FunctionComponent = () => (
         icon={IconBrandWikipedia}
         external
       >
-        Email
+        Wiki
       </NavLink>
 
       <LanguageSwitcher />
